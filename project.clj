fd3cef4bd58185c9a618fd1550f3ef2792064743
Sébastien Arnaud <arnaudsj@gmail.com>
2014-04-06 13:14:37 -0400

    Moving gyp-base into lein submodule to avoid recompiling all the java at each iteration.

--- project.clj
+++ project.clj
@@ -1,24 +1,14 @@
-(defproject ggp-clj-players "0.1.0"
+(defproject ggp-clj-players "0.1.1"
   :description "CLojure library to develop GGP players"
   :url "http://E"
   :license {:name "MIT license"
             :url "http://opensource.org/licenses/MIT"}
   :min-lein-version "2.0.0"
+  :plugins [[lein-sub "0.3.0"]]
+  :source-paths ["src"]
+  :sub ["vendor/ggp-base"]
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [junit/junit "4.11"]
-                 [org.json/json "20140107"]
-                 [org.apache.xmlgraphics/batik-transcoder "1.7"]
-                 [org.python/jython "2.7-b1"]
-                 [org.xhtmlrenderer/flying-saucer-core "9.0.4"]
-                 [nu.validator.htmlparser/htmlparser "1.4"]
-                 [com.google.guava/guava "16.0"]
-                 [org.reflections/reflections "0.9.9-RC1"]]
-  :source-paths      ["src/clj"]
-  :java-source-paths ["src/java"]
-  :javac-options     ["-target" "1.7" "-source" "1.7"] ; "-Xlint:deprecation"
-  ; :global-vars {*warn-on-reflection* true
-  ;               *assert* false}
-  ; :bootclasspath true
+                 [ggp-base "0.1.1"]]
   :profiles {:uberjar {:aot [ggp-clj-players.random]}}
   :aliases {"kiosk" ["do" "clean" ["uberjar"] ["run" "-m" "ggp-clj-players.core/kiosk"]]
             "launch" ["do" "clean" ["uberjar"] ["run" "-m" "ggp-clj-players.core/play"]]}
